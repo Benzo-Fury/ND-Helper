887ca66e9f2b9d73dd87a6308be1409b98d572e4
Neo Haggard <neohaggard07@gmail.com>
2025-10-18 08:25:16 +0000

    refractor: remove redundant code

--- src/config.ts
+++ src/config.ts
@@ -14,14 +14,8 @@ const config = {
     "https://creazilla-store.fra1.digitaloceanspaces.com/emojis/49889/package-emoji-clipart-md.png",
   rantEmoji: "🔔",
 
-  // Channels
-  neoRants: "1322042901965967440",
-
   // Users
   owner: "671610612475756576",
-
-  // Roles
-  rantListener: "1322044825100357642",
 
   // Command Ids (MODIFY ALL DISCORD IDS SO THEY UPDATE UPON CLIENT READY)
   presets: "1294083619912028272",
